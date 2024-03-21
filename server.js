42741d3b48abefb54c709f7d0b0cd80dfc8d8b3e
Raphael V. Bueno <rafavbueno@gmail.com>
2024-03-21 15:44:53 -0300

    Adiciona formularios ao html, servidor arrumado e armazenando as tarefas em um banco de dados

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-import { createServer } from 'node:http'
-import { readFile } from 'node:fs/promises'
+import express from 'express'
+import { MongoClient } from 'mongodb'
 
-const server = createServer(async (req, res) => {
-  console.log(req.method, req.url)
-  const html = await readFile('./primeiro_site.html')
-  res.writeHead(200, {
-    'content-type': 'text/html',
-  })
-  res.write(html)
-  res.end
-})
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', express.static('public'))
 
 const port = 3000
 
-server.listen(port, () => {
-  console.log('servidor rodando em http://localhost:' + port)
+const url = 'mongodb://localhost:27017'
+
+const dbname = 'todo-db'
+
+const client = new MongoClient(url)
+
+app.listen(port, () => {
+  console.log('Servidor rodando em http://localhost:' + port)
+})
+
+app.post('/add-tarefa', async (req, res) => {
+  try {
+    await client.connect()
+
+    console.log('Conectado com sucesso ao servidor')
+    console.log(req.body.tarefa)
+
+    const db = client.db(dbname)
+
+    const collection = db.collection('tarefas')
+
+    const result = await collection.insertOne({ tarefa: req.body.tarefa })
+
+    console.log(`Tarefa inserida com o _id: ${result.insertedId}`)
+
+    res.send('Tarefa: ' + req.body.tarefa + ' adicionada com sucesso!')
+  } catch (err) {
+    console.error(err)
+    res.send('Ocorreu um erro ao adicionar a tarefa')
+  } finally {
+    await client.close()
+  }
 })
