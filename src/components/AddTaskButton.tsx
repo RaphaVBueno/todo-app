9f913ba695ad744fe8f6a45a66e2954fc5c0963d
Will Amaral <willian.amaral@thoughtworks.com>
2024-09-05 22:09:07 -0300

    refactor: AddTaskButton

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -1,36 +1,18 @@
-import React from 'react'
-import { Button } from '@mui/material'
+import { Button, ButtonProps } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import Box from '@mui/material/Box'
 
-type AddTaskButtonProps = {
-  onClick: () => void
-}
+function AddTaskButton(props: ButtonProps) {
+  const { onClick } = props
 
-const AddTaskButton: React.FC<AddTaskButtonProps> = ({ onClick }) => {
   return (
-    <Box
-      sx={{
-        position: 'fixed',
-        right: 16,
-        left: 0,
-        top: '93%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
+    <Box sx={{ maxWidth: { md: '35%' }, mb: 4 }}>
       <Button
         variant="contained"
         color="primary"
         startIcon={<AddIcon />}
         onClick={onClick}
-        sx={{
-          //width: '25%', // para telas 720
-          //height: '32px', // para telas 720
-          width: '25%', // para telas de 1080
-          height: '40px', // para telas de 1080
-        }}
+        fullWidth
       >
         Adicionar tarefa
       </Button>
