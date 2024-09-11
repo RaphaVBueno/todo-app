bc9f64db6cfb735ffadf9da3e1945f8c98d127e1
ViniciusVBueno <vinivbueno.vb@gmail.com>
2024-09-11 11:39:00 -0300

    Adiciona menu ao botão Adicionar tarefa e inclui campos de entrada e autocompletes

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -1,21 +1,104 @@
-import { Button, ButtonProps } from '@mui/material'
+import { useState } from 'react'
+import { Button, ButtonProps, Menu, MenuItem, Box, TextField, Autocomplete } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
-import Box from '@mui/material/Box'
+
+const Categorias = [
+  { title: 'Tarefas de casa' },
+  { title: 'Tarefas do trabalho' },
+]
+
+const Tag = [
+  { title: 'Urgente' },
+  { title: 'Nao sei' },
+]
 
 function AddTaskButton(props: ButtonProps) {
   const { onClick } = props
 
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const isMenuOpen = Boolean(anchorEl)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <Box sx={{ maxWidth: { md: '35%' }, mb: 4 }}>
+    <Box sx={{ maxWidth: { md: '50%' }, mb: 4 }}>
       <Button
         variant="contained"
         color="primary"
         startIcon={<AddIcon />}
-        onClick={onClick}
+        onClick={handleClick}
         fullWidth
       >
         Adicionar tarefa
       </Button>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={isMenuOpen}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        slotProps={{
+          paper: {
+            sx: {
+              height: '500px',
+              width: anchorEl?.clientWidth || 'auto', // Largura igual ao botão
+              padding: '16px',
+              display: 'flex',
+              flexDirection: 'column',
+            },
+          },
+        }}
+      >
+        <MenuItem sx={{ p: 0, mt: 0 }}>
+          <TextField
+            id="outlined-basic"
+            label="Título da tarefa"
+            variant="outlined"
+            fullWidth
+          />
+        </MenuItem>
+        <MenuItem sx={{ p: 0, mt: '10px' }}>
+          <TextField
+            id="outlined-multiline"
+            label="Descrição"
+            variant="outlined"
+            multiline
+            rows={1} // Nao consegui deixar com 4 linhas esta bugado
+            fullWidth
+          />
+        </MenuItem>
+        <MenuItem sx={{ p: 0, mt: '10px' }}>
+          <Autocomplete
+            disablePortal
+            options={Categorias}
+            getOptionLabel={(option) => option.title}
+            renderInput={(params) => <TextField {...params} label="Categoria" />}
+            fullWidth
+          />
+        </MenuItem>
+        <MenuItem sx={{ p: 0, mt: '10px' }}>
+          <Autocomplete
+            disablePortal
+            options={Tag}
+            getOptionLabel={(option) => option.title}
+            renderInput={(params) => <TextField {...params} label="Tag" />}
+            fullWidth
+          />
+        </MenuItem>
+      </Menu>
     </Box>
   )
 }
--- src/components/Lista.tsx
+++ src/components/Lista.tsx
@@ -7,6 +7,7 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import Checkbox from '@mui/material/Checkbox'
 import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider'
 //import EditIcon from '@mui/icons-material/Edit'
 import { updateTaskStatus, queryClient } from '../utils'
 import { Task } from '../types'
@@ -34,54 +35,133 @@ export default function Lista(props: ListaProps) {
     mutate({ id, status })
   }
 
-  return (
-    <List
-      sx={{
-        width: '90%',
-        maxWidth: 1920,
-        bgcolor: 'background.paper',
-        borderRadius: '8px',
-      }}
-    >
-      {tasksList.map((task, index) => {
-        const labelId = `checkbox-list-label-${index}`
+  const todoTasks = tasksList.filter(task => !task.status)
+  const completedTasks = tasksList.filter(task => task.status)
 
-        return (
-          <ListItem
-            key={task.id}
-            secondaryAction={
-              <>
-                {/*<IconButton edge="end" aria-label="edit">
-                  <EditIcon />
-                </IconButton>*/}
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={task.status}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                  onChange={(event) => handleChange(event, task.id)}
-                />
-              </ListItemIcon>
-              <ListItemText
-                id={labelId}
-                primary={task.title}
-                secondary={task.dueDate}
-                sx={{ textDecoration: task.status ? 'line-through' : 'none' }}
-              />
-            </ListItemButton>
+  const handleListItemClick = (id: number, currentStatus: boolean) => {
+    mutate({ id, status: !currentStatus })
+  }
+
+  return (
+    <div>
+      {todoTasks.length > 0 && (
+        <List
+          sx={{
+            width: '90%',
+            maxWidth: 1920,
+            bgcolor: 'background.paper',
+            borderRadius: '8px',
+            mb: 2,
+          }}
+        >
+          <ListItem>
+            <ListItemText primary="Tarefas a Fazer" />
           </ListItem>
-        )
-      })}
-    </List>
+          {todoTasks.map((task, index) => {
+            const labelId = `checkbox-list-label-${index}`
+
+            return (
+              <ListItem
+                key={task.id}
+                secondaryAction={
+                  <>
+                    {/*<IconButton edge="end" aria-label="edit">
+                      <EditIcon />
+                    </IconButton>*/}
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </>
+                }
+                disablePadding
+              >
+                <ListItemButton
+                  role={undefined}
+                  dense
+                  onClick={() => handleListItemClick(task.id, task.status)}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={task.status}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                      onChange={(event) => handleChange(event, task.id)}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    id={labelId}
+                    primary={task.title}
+                    secondary={task.dueDate}
+                    sx={{ textDecoration: task.status ? 'line-through' : 'none' }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            )
+          })}
+        </List>
+      )}
+      {todoTasks.length > 0 && completedTasks.length > 0 && (
+        <Divider sx={{ my: 2 }} />
+      )}
+      {completedTasks.length > 0 && (
+        <List
+          sx={{
+            width: '90%',
+            maxWidth: 1920,
+            bgcolor: 'background.paper',
+            borderRadius: '8px',
+          }}
+        >
+          <ListItem>
+            <ListItemText primary="Tarefas Concluídas" />
+          </ListItem>
+          {completedTasks.map((task, index) => {
+            const labelId = `checkbox-list-label-${index}`
+
+            return (
+              <ListItem
+                key={task.id}
+                secondaryAction={
+                  <>
+                    {/*<IconButton edge="end" aria-label="edit">
+                      <EditIcon />
+                    </IconButton>*/}
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  </>
+                }
+                disablePadding
+              >
+                <ListItemButton
+                  role={undefined}
+                  dense
+                  onClick={() => handleListItemClick(task.id, task.status)}
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={task.status}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                      onChange={(event) => handleChange(event, task.id)}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    id={labelId}
+                    primary={task.title}
+                    secondary={task.dueDate}
+                    sx={{ textDecoration: task.status ? 'line-through' : 'none' }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            )
+          })}
+        </List>
+      )}
+    </div>
   )
 }
