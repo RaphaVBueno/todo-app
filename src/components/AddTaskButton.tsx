ed002d8949f30d6ca5f9578451a3c81fba6ba983
ViniciusVBueno <vinivbueno.vb@gmail.com>
2024-09-17 11:41:06 -0300

    Ajustes no visual do menu do botão Adicionar Tarefa

--- src/components/AddTaskButton.tsx
+++ src/components/AddTaskButton.tsx
@@ -1,75 +1,43 @@
 import { useState } from 'react'
-import {
-  Button,
-  Menu,
-  MenuItem,
-  Box,
-  TextField,
-  Autocomplete,
-} from '@mui/material'
+import { Button, Menu, MenuItem, Box, TextField, Autocomplete, Stack } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
-import * as React from 'react'
-import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import type { List } from '../types/list'
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar'
 
-const Tag = [{ title: 'Urgente' }, { title: 'Nao sei' }]
+const Categorias = [
+  { title: 'Tarefas de casa' },
+  { title: 'Tarefas do trabalho' },
+]
 
-type AddTaskButtonProps = {
-  categories: List[]
-}
+const Tag = [
+  { title: 'Urgente' },
+  { title: 'Nao sei' },
+]
 
-function AddTaskButton(props: AddTaskButtonProps) {
-  const { categories } = props
+function AddTaskButton() {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const isMenuOpen = Boolean(anchorEl)
   const [dueDate, setDueDate] = useState<Date | null>(null)
+  const isMenuOpen = Boolean(anchorEl)
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)
+  const handleClose = () => setAnchorEl(null)
 
   return (
-    <Box sx={{ maxWidth: { md: '40%' }, mb: 4 }}>
-      <Button
-        variant="contained"
-        color="primary"
-        startIcon={<AddIcon />}
-        onClick={handleClick}
-        fullWidth
-      >
+    <Box sx={{ maxWidth: { md: '50%' }, mb: 4 }}>
+      <Button variant="contained" color="primary" startIcon={<AddIcon />} onClick={handleClick} fullWidth>
         Adicionar tarefa
       </Button>
-
       <Menu
         anchorEl={anchorEl}
         open={isMenuOpen}
         onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        transformOrigin={{ vertical: 'bottom', horizontal: 'center' }}
         slotProps={{
           paper: {
-            sx: {
-              height: '680px',
-              width: anchorEl?.clientWidth || 'auto',
-              padding: '16px',
-              display: 'flex',
-              flexDirection: 'column',
-            },
+            sx: { height: '680px', width: anchorEl?.clientWidth || 'auto', padding: '10px', display: 'flex', flexDirection: 'column' },
           },
         }}
       >
@@ -79,6 +47,7 @@ function AddTaskButton(props: AddTaskButtonProps) {
             label="Título da tarefa"
             variant="outlined"
             fullWidth
+            sx={{ '& .MuiInputBase-root': { height: '44px', fontSize: '1.1rem' }, '& .MuiFormLabel-root': { fontSize: '1.1rem' } }}
           />
         </MenuItem>
         <MenuItem sx={{ p: 0, mt: '10px' }}>
@@ -87,51 +56,56 @@ function AddTaskButton(props: AddTaskButtonProps) {
             label="Descrição"
             variant="outlined"
             multiline
-            rows={1} // Nao consegui deixar com 4 linhas esta bugado
+            rows={1}
             fullWidth
+            sx={{ '& .MuiInputBase-root': { height: '44px', fontSize: '1.1rem' }, '& .MuiFormLabel-root': { fontSize: '1.1rem' } }}
           />
         </MenuItem>
         <MenuItem sx={{ p: 0, mt: '10px' }}>
           <Autocomplete
             disablePortal
-            options={categories}
-            getOptionLabel={(option) => option.name}
-            renderInput={(params) => (
-              <TextField {...params} label="Categoria" />
-            )}
+            options={Categorias}
             fullWidth
+            getOptionLabel={(option) => option.title}
+            renderInput={(params) => (
+              <TextField {...params} label="Categoria" sx={{ '& .MuiInputBase-root': { height: '44px', fontSize: '1.1rem' }, '& .MuiFormLabel-root': { fontSize: '1.1rem' } }} />
+            )}
           />
         </MenuItem>
-        <MenuItem sx={{ p: 0, mt: '10px', mb: '50px' }}>
+        <MenuItem sx={{ p: 0, mt: '10px', mb: '30px' }}>
           <Autocomplete
             disablePortal
+            fullWidth
             options={Tag}
             getOptionLabel={(option) => option.title}
-            renderInput={(params) => <TextField {...params} label="Tag" />}
-            fullWidth
+            renderInput={(params) => (
+              <TextField {...params} label="Tag" sx={{ '& .MuiInputBase-root': { height: '44px', fontSize: '1.1rem' }, '& .MuiFormLabel-root': { fontSize: '1.1rem' } }} />
+            )}
           />
         </MenuItem>
         <MenuItem>
-          <LocalizationProvider
-            dateAdapter={AdapterDateFns}
-            adapterLocale={ptBR}
-          >
+          <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
             <DateCalendar
               value={dueDate}
-              label={
-                dueDate == null
-                  ? null
-                  : format(dueDate, 'dd MMM, yyyy', { locale: ptBR })
-              }
-              onChange={(newValue) => setDueDate(newValue)}
-              open={true}
+              onChange={setDueDate}
               sx={{
-                width: '300px',
-                scale: '1.3',
+                width: '360px',
+                '& .MuiPickersArrowSwitcher-root': { justifyContent: 'space-between', '& button': { ml: '4px' } },
+                '& .MuiTypography-root': { fontSize: '1.1rem', margin: '0px 5px' }, // Dia da semana
+                '& .MuiPickersCalendarHeader-label': { fontSize: '1.2rem' }, // Mês
+                '& .MuiPickersDay-root': { fontSize: '1.1rem', margin: '4px 5px' }, // Dia do mês
               }}
             />
           </LocalizationProvider>
         </MenuItem>
+        <Stack direction="row" spacing={4} justifyContent="center" sx={{ mt: '17px' }}>
+          <Button variant="outlined" color="primary" onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleClose}>
+            Salvar
+          </Button>
+        </Stack>
       </Menu>
     </Box>
   )
