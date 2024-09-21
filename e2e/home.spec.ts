699e4815f9ffa337eb6df4a23c1974ea652ad910
Will Amaral <willian.amaral@thoughtworks.com>
2024-09-20 23:01:23 -0300

    adiciona testes e2e playwright

---
+++ e2e/home.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test'
+// Arrange (preparar), Act (agir), Assert (verificar)
+test('Jornada - CRUD tarefa', async ({ page, browserName }) => {
+  // Arrange - configurar stubs de teste e ir para a página a ser testada
+  const taskName = `jogar astrobot ${browserName}`
+  await page.goto('http://localhost:5173/')
+  //---
+  // Act (Create) - Adicionei uma tarefa
+  await page.getByRole('button', { name: 'Adicionar tarefa' }).click()
+  await page.getByLabel('Título da tarefa').fill(taskName)
+  await page.getByRole('button', { name: 'Salvar' }).click()
+  // Assert (Read) - Verifiquei se a tarefa está visível
+  await expect(page.getByText(taskName)).toBeVisible()
+  // await expect()... verificar se a tarefa está marcada como feita ou não - padrão não marcado
+  // ---
+  // Act (Update) - Marcar a tarefa como feita
+  await page.getByText(taskName).click()
+  // Assert - Tarefa marcada como feita
+  // await expect()... encontrar o localizador da tarefa e de alguma forma verificar se o checkbox está marcado ou não.
+  // Adicionalmente, verificar se o texto está sobrescrito
+  // Act (Update) - Marcar a tarefa novamente como não feita
+  await page.getByText(taskName).click()
+  // await expect()... mesmo estado de quando a tarefa foi criada ap
+  // ---
+  // Act (Delete) - Deletar a tarefa
+  await page
+    .locator('li')
+    .filter({ hasText: taskName })
+    .getByLabel('delete')
+    .click()
+  // Assert - Tarefa não é mais existente na página
+  await expect(page.getByText(taskName)).not.toBeVisible()
+})
