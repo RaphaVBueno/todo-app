cad1129f23d69151aabf324ca92a923bf0093ab8
Will Amaral <willian.amaral@thoughtworks.com>
2024-10-18 20:43:57 -0300

    fix type error

--- src/types/dashboard.ts
+++ src/types/dashboard.ts
@@ -1,10 +1,8 @@
 import type { Dispatch, SetStateAction } from 'react'
-import { Task } from './task'
 
 export type DashboardContext = {
   filter: number | null
   setFilter: Dispatch<SetStateAction<number | null>>
   date: Date | null
   setDate: Dispatch<SetStateAction<Date | null>>
-  searchList: Task[] | null
 }
