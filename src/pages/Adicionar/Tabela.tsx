d434444b33ba6801249316de98f7bbe132f9c493
ViniciusVBueno <vinivbueno.vb@gmail.com>
2024-11-07 18:26:37 -0300

    Adicionado a cor da categoria

--- src/pages/Adicionar/Tabela.tsx
+++ src/pages/Adicionar/Tabela.tsx
@@ -1,6 +1,7 @@
 import { List } from '@/types'
 import { DataGrid } from '@mui/x-data-grid'
 import EditTagButtons from './EditTagButtons'
+import Brightness1Icon from '@mui/icons-material/Brightness1'
 
 type TabelaProps = {
   categories: List[] | undefined
@@ -20,12 +21,34 @@ const colunaTag = [
     headerName: 'Quantidade',
     width: 150,
     disableColumnMenu: true,
+    headerAlign: 'center', // Centraliza o nome da coluna
+    align: 'center', // Centraliza o conteúdo da célula
   },
   {
     field: 'color',
     headerName: 'Cor',
     width: 100,
     disableColumnMenu: true,
+    headerAlign: 'center', // Centraliza o nome da coluna
+    align: 'center', // Centraliza o conteúdo da célula
+    renderCell: (params: any) => (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        {/* Alinha o ícone no centro da célula */}
+        <Brightness1Icon
+          style={{
+            color: `${params.row.color}`,
+            fontSize: '16px',
+          }}
+        />
+      </div>
+    ),
   },
   {
     field: 'actions',
@@ -36,6 +59,8 @@ const colunaTag = [
     renderCell: (params: any) => (
       <EditTagButtons name={params.row.name} listId={params.row.id} />
     ),
+    headerAlign: 'center',
+    align: 'center',
   },
 ]
 
